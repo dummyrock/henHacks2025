d1b5516d4745012c5ea95536a3a0bba535470748
Ever Merino <emjr@udel.edu>
2025-03-01 21:07:04 -0500

    get username and password functions added, but still gives error as the submit button sends you somewhere else

--- healthProject/src/app/main.component.ts
+++ healthProject/src/app/main.component.ts
@@ -1,6 +1,6 @@
 import html from "./main.component.html";
 import css from "./main.component.css";
-import { EzComponent, Click, EventSubject, BindCSSClass, BindStyle } from '@gsilber/webez';
+import { EzComponent, Click, EventSubject,BindValue, BindCSSClass, BindStyle } from '@gsilber/webez';
 import { UserHomepageComponent } from "../user_home/user_home.component";
 
 /**
@@ -15,6 +15,10 @@ export class MainComponent extends EzComponent {
     @BindStyle("new-user", "display")
     public newDisplay: string = "none"
     private userHomepage: UserHomepageComponent = new UserHomepageComponent();
+    @BindValue("UserID")
+    private username: string = ""
+    @BindValue("KeyID")
+    private password: string = ""
 
     constructor() {
         super(html, css);
@@ -30,6 +34,16 @@ export class MainComponent extends EzComponent {
     showSignup(){
         this.returningDisplay = "none"
         this.newDisplay = "block"
+    }
+    @Click("submit")
+    getUsername(): string{
+        console.log(this.username);
+        return this.username;
+    }
+    @Click("submit")
+    getPassword(): string {
+        console.log(this.password);
+        return this.password;
     }
 }
 
